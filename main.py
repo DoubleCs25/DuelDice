554c098fe06bad930b99ef128046a6724b2b9390
gold <ccunningham@sjnma.org>
2017-03-11 02:34:41 -0600

    [ finished project]

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 from random import randrange
-print("We are going to play Dice Duels, there will be three rounds")
 
 win=3
 plyr1roll=0
 plyr2roll=0
-
+	
 plyr2win=0
 plyr1win=0
-
+	
 while(plyr1win != win and plyr2win != win):
-	plyr1roll=randrange(1,6)
-	print(plyr1roll)
-	plyr2roll=randrange(1,6)
-	print(plyr2roll)
-	if(plyr1roll>plyr2roll):
-		plyr1win+= 1
-		print("plyr1, you win this one")
+    plyr1roll=randrange(1,6)
+    print(plyr1roll)
+    plyr2roll=randrange(1,6)
+    print(plyr2roll)
 
-	if(plyr2roll>plyr1roll):
-		plyr2win+= 1
-		print("plyr2, you win this one")
+    if(plyr1roll>plyr2roll):
+        plyr1win+= 1
+        print("plyr1, you win this one")
+	
+    if(plyr2roll>plyr1roll):
+        plyr2win+= 1
+        print("plyr2, you win this one")
+	
+    if(plyr1win==3):
+        print("plyr1, you win balla")
+    if(plyr2win==3):
+        print("plyr2, you win homie")
 
-	if(plyr1win==3):
-		print("plyr1, you win bud")
-	if(plyr2win==3):
-		print("plyr2, you win bud")
+    if(plyr1roll==5):
+        print("plyr1, you win balla")
+        plyr1win+= 3
+
+    if(plyr2roll==5):
+        print("plyr2, you win homie")
+        plyr2win+= 3
+
+    if(plyr1win== win):
+        print("congrats player 1 you win")
+    if(plyr2win== win):
+        print("congrats player 2 you win")
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-1. The lowest score of each round wins
-2. If your name has an A in it, you get 3 more points
-3. But if your name has an S in it, you lose 2 points
-4. If your score is twice the amount of your opponents, you automatically lose
-
+1. The highest score of each round wins
+2. If a player rolls a 5, they win
+3. If both players roll the same number, roll again
